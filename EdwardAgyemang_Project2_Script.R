f76fa65cc9cea3855d451b9fc21dde104928e35e
aedward883-blip <aedward883@gmail.com>
2026-04-18 10:04:00 -0700

    Add Project 2 R script, datasets, histogram and test file

---
+++ EdwardAgyemang_Project2_Script.R
@@ -0,0 +1,11 @@
+# Name: Edward Agyemang | Date: 2024 | Class: ALY 6000
+# ============================================================
+# ALY 6000 - Project 2: Exploratory Data Analysis
+# ============================================================
+
+rm(list = ls())
+setwd("C:/Users/elitebook folio/Documents/EDWARD PROJECTS/ALY6000/Project 2")
+
+library(pacman)
+p_load(tidyverse, janitor, testthat)
+
